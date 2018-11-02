f7b11c520113dbbc6d2f51726ecb8904a93f2465
dartexlt <dorition@gmail.com>
2018-11-03 00:01:07 +0200

    input form created

---
+++ resources/views/inputform.blade.php
@@ -0,0 +1,17 @@
+<div class="form-row" >
+			<div class="form-group col-sm-1">
+				<input class="form-check-input center" type="checkbox" value="" id={{$month}}>
+			</div>
+			<div class="form-group col-sm-1">
+				<label for={{$month}} class="col-sm-1 col-form-label col-form-label-sm">{{$month}}</label>
+			</div>
+			<div class="col-sm-1">
+				<input type="text" class="form-control form-control-sm" id={{$month}} placeholder={{$month}}>
+			</div>
+			<div class="col-sm-1">
+				<input type="text" class="form-control form-control-sm" id={{$month}} placeholder={{$month}}>
+			</div>
+			<div class="col-sm-1">
+				<input type="text" class="form-control form-control-sm" id={{$month}} placeholder={{$month}}>
+			</div>
+		</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,40 @@
 @extends('main')
 @section('title', 'Home')
 @section('content')
-
+<form>
+	<div class="container">
+		<div class ="row">
+			<div class="col-md-1">
+				<label>Heating season</label>
+			</div>
+			<div class="col-md-1">
+				<label>Month</label>
+			</div>
+			<div class="col-md-1">
+				<label>Transferred heat to network</label>
+			</div>
+			<div class="col-md-1">
+				<label>Hours</label>
+			</div>
+			<div class="col-md-1">
+				<label>Average outdoor temperature</label>
+			</div>
+		</div>
+		@include('inputform',['month' => 'January'])
+		@include('inputform',['month' => 'February'])
+		@include('inputform',['month' => 'March'])
+		@include('inputform',['month' => 'April'])
+		@include('inputform',['month' => 'May'])
+		@include('inputform',['month' => 'June'])
+		@include('inputform',['month' => 'July'])
+		@include('inputform',['month' => 'August'])
+		@include('inputform',['month' => 'September'])
+		@include('inputform',['month' => 'October'])
+		@include('inputform',['month' => 'Novenber'])
+		@include('inputform',['month' => 'December'])
+		<div class="row" >
+			<button type="button" class="btn btn-primary btn-md">Calculate</button>
+		</div>
+	</div>
+</form>
 @endsection
